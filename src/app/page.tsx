4edbcee95abf60086ce9a6e7a09cee2ec4fecb3d
Barnil Sarma <barnilsarma@gmail.com>
2026-03-04 00:27:01 +0530

    peoples section initial done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import * as components from "../components";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#e4f8ff] to-[rgba(0, 148, 202, 0)] text-white">
+    <main className="overflow-hidden flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#e4f8ff] to-[rgba(0, 148, 202, 0)] text-white">
       <components.Home.Hero/>
+      <components.Home.people/>
     </main>
   );
 }
